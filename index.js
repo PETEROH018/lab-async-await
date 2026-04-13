8580e1cced2fbd5b22c4a071c91d5176a6d60398
PETEROH018 <mathengep63@gmail.com>
2026-04-13 17:32:35 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here!
+async function handleFetch(){
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await response.json()
+    displayPosts(posts)
+}
+
+function displayPosts(posts){
+    const postsList = document.querySelector('#post-list')
+    posts.forEach((post) => {
+    const list = document.createElement('li')
+    const heading = document.createElement('h1')
+    heading.textContent = post.title
+    const paragraph = document.createElement('p')
+    paragraph.textContent = post.body
+    list.appendChild(heading)
+    list.appendChild(paragraph)
+    postsList.appendChild(list)
+
+ 
+
+    })
+
+}
+
+handleFetch()
